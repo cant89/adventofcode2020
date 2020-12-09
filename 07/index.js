1f27c283a2b7e61cacff2970e7bd6965deaa2a8c
Davide Cantelli <cant1989@gmail.com>
2020-12-10 00:20:10 +0100

    day 07 completed

--- 07/index.js
+++ 07/index.js
@@ -1,29 +1,62 @@
 const { importData } = require("../utils");
 
+function getContained(rules, type) {
+  const regex = new RegExp(`(.*) bags contain.*[0-9] ${type}`, "gm");
+  const match = rules.match(regex);
+
+  if (match) {
+    return match
+      .map((rule) => {
+        const [adj, color] = rule.split(" ");
+        const biggerBag = `${adj} ${color}`;
+        const containsBiggerBag = getContained(rules, biggerBag);
+        return [biggerBag, ...containsBiggerBag];
+      })
+      .flat();
+  }
+
+  return [];
+}
+
 function getContain(rules, type) {
-  return rules.reduce((acc, rule) => {
-    const regex = new RegExp(`contain.*${type}`, "gm");
-    const match = rule.match(regex);
-    if (match) {
-      const [, biggerBag] = /(.*)\sbags contain/gm.exec(rule);
-      const containsBiggerBag = getContain(rules, biggerBag);
-      return [...acc, biggerBag, ...containsBiggerBag];
-    }
-    return acc;
-  }, []);
+  const regex = new RegExp(`${type} bags contain (.*).`, "g");
+  const [, contained] = regex.exec(rules) || [];
+
+  if (contained && contained !== "no other bags") {
+    const bags = contained.split(", ");
+
+    const total = bags.map((bag) => {
+      const [amount, adj, color] = bag.split(" ");
+      const amountOfContained = getContain(rules, `${adj} ${color}`);
+      return Number(amount) * amountOfContained;
+    });
+
+    return total.reduce((a, b) => a + b, !Boolean(type === "shiny gold"));
+  }
+
+  return 1;
 }
 
 function getPartOne(rules) {
-  const containsShinyGold = getContain(rules, "shiny gold");
-  const uniq = [...new Set([containsShinyGold].flat(2))];
+  const containsShinyGold = getContained(rules, "shiny gold");
+  const uniq = [...new Set(containsShinyGold)];
+
   return uniq.length;
 }
+
+function getPartTwo(rules) {
+  const containedInShinyGold = getContain(rules, "shiny gold");
+
+  return containedInShinyGold;
+}
+
 async function run() {
-  const data = (await importData("07/data.txt")).split(/\n/gm);
+  const data = await importData("07/data.txt");
 
   const res1 = getPartOne(data);
+  const res2 = getPartTwo(data);
 
-  console.log(res1);
+  console.log(res1, res2);
 }
 
 run();
